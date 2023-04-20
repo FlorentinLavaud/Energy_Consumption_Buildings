d81f3c1988561bce6a06b7802df369f1bd3cc706
florentin lavaud <florentin.lavo@gmail.com>
2023-04-20 21:57:09 +0200

    debug lib

--- lib/__ini__.py
+++ lib/__ini__.py
@@ -1,10 +1,3 @@
 """ 
 lib/__ini__.py: initilization file for the lib directory
 """
-import pandas as pd 
-pd.set_option('display.max_columns', None)
-
-import os 
-path = os.getcwd()
-path_data = path+'/Building_Energy_TRAIN.xlsx'
-pd.read_excel(path+'/Building_Energy_TRAIN.xlsx')
